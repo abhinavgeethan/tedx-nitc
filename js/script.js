7743c22843a744e09060e27db5252dd98b1dba0e
Nikhil Nath R <nikhilnath1000@gmail.com>
2020-02-28 08:20:03 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,59 @@
+
+let headerElem = document.querySelector("header");
+let menuBtnElem = document.querySelector("header .menu-btn");
+let navElem = document.querySelector("nav");
+
+const fixedScrollValue = 20;
+
+let isScrolled = false;
+let isMenuActive = false;
+
+
+menuBtnElem.addEventListener("click", () => {
+  if (isMenuActive) {
+    isMenuActive = false;
+    hideNav();
+  } else {
+    isMenuActive = true;
+    showNav();
+  }
+});
+
+navElem.addEventListener("click", e => {
+  if (e.target == navElem) {
+    isMenuActive = false;
+    hideNav();
+  }
+});
+
+document.addEventListener("scroll", e => {
+  let offset = window.pageYOffset;
+
+  if (isScrolled && offset < fixedScrollValue) {
+    isScrolled = false;
+    headerElem.style.transform = "translateY(20px)";
+    headerElem.style.backgroundColor = "#00000000";
+    headerElem.style.position = "relative";
+  } else if (!isScrolled && offset >= fixedScrollValue) {
+    isScrolled = true;
+    headerElem.style.transform = "translateY(0px)";
+    headerElem.style.backgroundColor = "#161616";
+    headerElem.style.position = "fixed";
+  }
+});
+
+function showNav() {
+  navElem.classList.add("active");
+  menuBtnElem.classList.add("active");
+  setTimeout(() => {
+    navElem.style.opacity = 1;
+  }, 20);
+}
+
+function hideNav() {
+  navElem.style.opacity = "";
+  menuBtnElem.classList.remove("active");
+  setTimeout(() => {
+    navElem.classList.remove("active");
+  }, 300);
+}
