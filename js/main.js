deea90cd43e9bbce85ef17c768862cd25ec40fb2
Nikhil Nath R <nikhilnath1000@gmail.com>
2020-02-29 21:56:38 +0000

    Godown animated arrow added

--- js/main.js
+++ js/main.js
@@ -1,29 +1,29 @@
 let bannerTexts = [
-  // {
-  //   name: "A change to pursue life", 
-  //   space: [1, 8, 18],
-  //   red: [19, 20, 21, 22],
-  //   breaks: [11],
-  // },
-  // {
-  //   name: "To do what's impossible",
-  //   space: [2, 5],
-  //   red: [13, 14, 15, 16, 17, 18, 19, 20, 21, 22],
-  //   breaks: [12],
-  // },
-  // {
-  //   name: "To say I can't can",
-  //   space: [2, 6, 14],
-  //   strike: true,
-  //   red: [15, 16, 17],
-  //   breaks: [8],
-  // },
-  // {
-  //   name: "To take roads not taken",
-  //   space: [2, 7, 17],
-  //   red: [8, 9, 10, 11, 12],
-  //   breaks: [13],
-  // },
+  {
+    name: "A change to pursue life", 
+    space: [1, 8, 18],
+    red: [19, 20, 21, 22],
+    breaks: [11],
+  },
+  {
+    name: "To do what's impossible",
+    space: [2, 5],
+    red: [13, 14, 15, 16, 17, 18, 19, 20, 21, 22],
+    breaks: [12],
+  },
+  {
+    name: "To say I can't can",
+    space: [2, 6, 14],
+    strike: true,
+    red: [15, 16, 17],
+    breaks: [8],
+  },
+  {
+    name: "To take roads not taken",
+    space: [2, 7, 17],
+    red: [8, 9, 10, 11, 12],
+    breaks: [13],
+  },
   {
     name: "See the unseen PERSPECTIVE",
     space: [3, 7],
